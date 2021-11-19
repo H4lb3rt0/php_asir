5c592d24a6eb42eb099154684db9b344a09729fc
albert0aliaga <94705612+albert0aliaga@users.noreply.github.com>
2021-11-19 19:21:54 +0100

    Add files via upload

---
+++ paroimpar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+                
+            $cont = 1;
+                    
+            $div = 2;
+            
+            while ($cont <= 100) {
+            
+            if($cont % $div == 0) echo $cont.' es un número par.<br>';
+            
+            else echo $cont.' es un número impar.<br>';
+            
+            $cont++;
+           
+            }
+            
+        ?>
+    </body>
+</html>
+
